d0ed7a9278d088d2022ec99215cddff2735020a2
cgonzaru <cristina.glez.rubio@gmail.com>
2019-12-21 12:25:54 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app__header">
+          
+        </header>
+        <main className="app__main">
+          :D
+        </main>
+        <footer className="app__footer">
+          
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
